db4540932cb5dfab49f07f6adebd3d7aa93c7bb2
Ricardo Del Rio Rubial <ricardo@neolpar.com>
2020-12-30 01:39:48 +0000

    added swagger / openapi to create documentations

--- PromocodesApp/Startup.cs
+++ PromocodesApp/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using PromocodesApp.Models;
 using System;
 using System.Collections.Generic;
@@ -28,6 +29,11 @@ namespace PromocodesApp
                 opt.UseInMemoryDatabase("PromocodesAppDB"));
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PromocodesApp", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +42,12 @@ namespace PromocodesApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PromocodesApp v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
